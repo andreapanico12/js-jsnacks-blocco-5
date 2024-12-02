a022f9fca64cd3ccac6caa633bb26552dc11de99
andreapanico12 <andreapanico12@gmail.com>
2024-12-02 14:11:26 +0100

    feat: add solutions of snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,14 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+console.log(names.join());
+
+
+// ====== prova forEach
+
+names.forEach((name)=>{
+  console.log(name)
+})
+
