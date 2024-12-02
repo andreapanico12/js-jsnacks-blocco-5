b6af78d968c5c69cef3cb4637c02a8eb09feb9ab
andreapanico12 <andreapanico12@gmail.com>
2024-12-02 14:46:43 +0100

    feat: add solution of snack 5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,21 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// ciclo for of
+
+
+let evenNums = [];
+
+for (let num of nums){
+  
+  if ( num % 2 === 0)
+    evenNums.push(num)
+}
+
+console.log(evenNums);
+
+// prova filter
+
+let evenNumsFilter = nums.filter(num => num % 2 === 0);
+console.log(evenNumsFilter);
